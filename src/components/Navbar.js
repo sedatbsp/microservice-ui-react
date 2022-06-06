fadd99b192b982767aa75986efa48b3976385397
Sedat Başpınar <sdtbsp10@gmail.com>
2022-06-06 20:56:54 +0300

    navigation bar edits

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,21 @@
-import { NavLink } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 import logo from '../logo.svg'
+import { clearCurrentUser } from '../redux/actions/user';
+import { Role } from '../models/Role';
 
 const Navbar = () => {
+
+    const currentUser = useSelector(state => state.user);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const logout = () => {
+        dispatch(clearCurrentUser());
+        navigate('/login');
+    }
+
+
     return (
         <nav className="navbar navbar-expand navbar-dark bg-dark">
             <NavLink to={'/home'} className="navbar-brand ms-1">
@@ -9,23 +23,38 @@ const Navbar = () => {
             </NavLink>
 
             <div className='navbar-nav me-auto'>
-                <li className='nav-item'>
-                    <NavLink to={'/admin'} className='nav-link'>Admin</NavLink>
-                </li>
+                {currentUser?.role === Role.ADMIN &&
+                    <li className='nav-item'>
+                        <NavLink to={'/admin'} className='nav-link'>Admin</NavLink>
+                    </li>
+                }
+
                 <li className='nav-item'>
                     <NavLink to={'/home'} className='nav-link'>Home</NavLink>
                 </li>
             </div>
 
-            <div className='navbar-nav ms-auto'>
-                <li className='nav-item'>
-                    <NavLink to={'/register'} className='nav-link'>Sign Up</NavLink>
-                </li>
-                <li className='nav-item'>
-                    <NavLink to={'/login'} className='nav-link'>Sign In</NavLink>
-                </li>
-            </div>
+            {!currentUser &&
+                <div className='navbar-nav ms-auto'>
+                    <li className='nav-item'>
+                        <NavLink to={'/register'} className='nav-link'>Sign Up</NavLink>
+                    </li>
+                    <li className='nav-item'>
+                        <NavLink to={'/login'} className='nav-link'>Sign In</NavLink>
+                    </li>
+                </div>
+            }
 
+            {currentUser &&
+                <div className='navbar-nav ms-auto'>
+                    <li className='nav-item'>
+                        <NavLink to={'/profile'} className='nav-link'>{currentUser.name}</NavLink>
+                    </li>
+                    <li className='nav-item'>
+                        <a href="#" onClick={() => logout()} className="nav-link">Logout</a>
+                    </li>
+                </div>
+            }
 
         </nav>
     )
